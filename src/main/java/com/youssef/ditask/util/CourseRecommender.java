80b7c4230ec6f39c6af495e700c5db41ff73ec76
Youssef Negm <youssef.negm2001@gmail.com>
2025-04-12 22:38:22 +0200

    feat: added course recommenders

---
+++ src/main/java/com/youssef/ditask/util/CourseRecommender.java
@@ -0,0 +1,11 @@
+package com.youssef.ditask.util;
+
+import com.youssef.ditask.model.Course;
+
+import java.util.List;
+
+public interface CourseRecommender {
+
+    List<Course> recommendedCourses();
+
+}
