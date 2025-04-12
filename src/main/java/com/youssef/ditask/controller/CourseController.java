200f83e35d401e06df12820e229521f3a444756b
Youssef Negm <youssef.negm2001@gmail.com>
2025-04-12 23:37:09 +0200

    feat: added course REST controller

---
+++ src/main/java/com/youssef/ditask/controller/CourseController.java
@@ -0,0 +1,32 @@
+package com.youssef.ditask.controller;
+
+import com.youssef.ditask.model.Course;
+import com.youssef.ditask.service.CourseRecommenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    private final CourseRecommenderService courseRecommenderService;
+
+    @Autowired
+    public CourseController(CourseRecommenderService courseRecommenderService) {
+        this.courseRecommenderService = courseRecommenderService;
+    }
+
+    @GetMapping("/{qualifier}")
+    public ResponseEntity<List<Course>> getCourses(@PathVariable String qualifier) {
+        return switch (qualifier) {
+            case "primary" -> ResponseEntity.ok(courseRecommenderService.getPrimaryRecommendedCourses());
+            case "qualifier" -> ResponseEntity.ok(courseRecommenderService.getQualifierRecommendedCourses());
+            default -> ResponseEntity.badRequest().build();
+        };
+    }
+
+}
