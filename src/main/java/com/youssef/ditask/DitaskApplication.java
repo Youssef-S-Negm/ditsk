af123b994c733d56b0008ebc68b0efd2b678c806
Youssef Negm <youssef.negm2001@gmail.com>
2025-04-12 22:17:44 +0200

    inital commit

---
+++ src/main/java/com/youssef/ditask/DitaskApplication.java
@@ -0,0 +1,13 @@
+package com.youssef.ditask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DitaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DitaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youssef/ditask/DitaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssef.ditask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DitaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
